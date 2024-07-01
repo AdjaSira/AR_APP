fb458145f37eadb9ce84a8013ac1f252565fd3d2
Fatou Fall Ndiaye <aafaatound@gmail.com>
2024-07-01 08:42:31 +0000

    ajustements des poppups

--- components/menue.js
+++ components/menue.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 import { Navbar } from "flowbite-react";
 
+
 const Menue = () => {
   const [isFormVisible, setFormVisible] = useState(false);
 
   const handleToggle = () => {
     setFormVisible(!isFormVisible);
   };
+
+  const [currentImage,setCurrentImage] = useState("/iPhone_15_Pro_Blue_Titanium_1_0.jpg");
+  const imageChange = (newImage) => {
+    setCurrentImage(newImage);
+  };
+
+
+
 
   return (
     <div className="relative max-w-screen justify-between flex flex-wrap items-center ml-6">
@@ -25,10 +34,10 @@ const Menue = () => {
           <Navbar.Link href="/navbars" active={true}>
             Home
           </Navbar.Link>
-          <Navbar.Link href="/navbars">iPhone 15</Navbar.Link>
-          <Navbar.Link href="/navbars">iPad Pro Max</Navbar.Link>
-          <Navbar.Link href="/navbars">Galaxy Z Fold</Navbar.Link>
-          <Navbar.Link href="/navbars">Galaxy Z Flip</Navbar.Link>
+          <Navbar.Link href="#"  onClick={() => imageChange("/téléchargement (2).jpeg") }>iPhone 15</Navbar.Link>
+          <Navbar.Link href="#"  onClick={() => imageChange("/iPhone_15_Pro_Blue_Titanium_1_0.jpg") } >iPad Pro Max</Navbar.Link>
+          <Navbar.Link href="#"  onClick={() => imageChange("/") } >Galaxy Z Fold</Navbar.Link>
+          <Navbar.Link href="#"  onClick={() => imageChange("/") }>Galaxy Z Flip</Navbar.Link>
         </Navbar.Collapse>
         <div className="block sm:hidden ml-auto">
           <Navbar.Toggle />
@@ -38,21 +47,13 @@ const Menue = () => {
         className="mt-2 ml-96 py-2 px-2 text-gray-900 focus:outline-none text-white bg-orange-400 hover:bg-orange-500 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-8 py-2.5 me-2 mb-2 dark:focus:ring-orange-900"
       >
         <button onClick={handleToggle}>Suivre Précommande</button>
+       
       </div>
-      {isFormVisible && (
+      {isFormVisible && (                                                                                                                             
         <div className="ml-60 absolute top-0 left-1/2 transform -translate-x-1/2 mt-4 p-4 border rounded-lg shadow-lg bg-white z-50">
           <form >
             <div className="mb-4">
-              <label htmlFor="phone" className="block text-gray-700">
-                Numéro de téléphone
-              </label>
-              <input
-                type="text"
-                id="phone"
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-orange-300 focus:border-orange-300"
-              />
-            </div>
-            <div className="mb-4">
+            <button className="bg-orange-400 w-10 ml-40" onClick={handleToggle}>X</button>
               <label htmlFor="orderNumber" className="block text-gray-700">
                 Numéro de précommande
               </label>
--- pages/index.js
+++ pages/index.js
@@ -26,10 +26,10 @@ export default function Home() {
         </div>
         <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 md:static md:ml-96 md:mt-10 md:bottom-auto">
           <button
-            className="border-gray-400 rounded mt-36"
+            className="border-gray-400 rounded mt-36 px-5"
             onClick={togglePoppup}
           >
-            <span className="py-5 px-4 text-gray-900 focus:outline-none text-white bg-orange-400 hover:bg-orange-500 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-8 py-2.5 me-2 mb-2 dark:focus:ring-orange-900">
+            <span className="py-7 px-16 text-gray-900 focus:outline-none text-white bg-orange-500 hover:bg-orange-500 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-8 py-2.5 me-2 mb-2 dark:focus:ring-orange-900">
               Précommander
             </span> 
           </button>
@@ -58,13 +58,25 @@ export default function Home() {
       {poppupVisible && (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
           <Poppup trigger={poppupVisible} setTrigger={setPoppupVisible}>
-            <h2 className="text-orange-500">Numéro de Téléphone :</h2>
-            <br />
-            <input className="border border-orange-300" type="text" placeholder="Entrez votre numéro..." />
+            <h2 className="text-orange-500">Nom :</h2>
+            <input className="border border-orange-300" type="text" placeholder="Nom..." />
+            <br /><br />
+            <h2 className="text-orange-500">Prénom :</h2>  
+            <input className="border border-orange-300" type="text" placeholder="Prénom..." />
+            <br /><br />
+            <h2 className="text-orange-500">Numéro de Télèphone :</h2>
+            <input className="border border-orange-300" type="text" placeholder="Numéro..." />
             <br /><br />
             <ColorPicker />
             <br /><br />
             <Memory />
+            <br /><br />
+            <h2 className="text-orange-500">Choisissez votre option de paiement :</h2>
+            <input type='radio' name='paiement' id='tontine' /> 
+            <label htmlFor='tontine' className="ml-2 text-gray-700">Tontine</label>
+              <br/>
+            <input type='radio' name='paiement' id='paiement-unique' /> 
+            <label htmlFor='paiement-unique' className="ml-2 text-gray-700">Paiement en une fois</label>
             <br /><br />
             <button className="text-white bg-gradient-to-r from-orange-500 via-orange-600 to-orange-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-orange-300 shadow-lg shadow-orange-500/50 dark:shadow-lg dark:shadow-orange-800/80 font-medium rounded-lg text-sm px-5 py-2 text-center me-2 mb-2">
               Suivant
